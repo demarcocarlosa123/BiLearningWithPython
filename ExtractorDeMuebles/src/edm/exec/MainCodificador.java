6dd54df5c83051a9ce2b1cb382a916ff0cc193a7
carlos.demarco <carlos.demarco@746pin008771.ar.infra.d>
2015-06-23 08:22:50 -0300

    Primer intento de codificador

---
+++ ExtractorDeMuebles/src/edm/exec/MainCodificador.java
@@ -0,0 +1,20 @@
+package edm.exec;
+
+import java.io.IOException;
+
+import edm.back.extractor.Extractor;
+import edm.back.extractor.ExtractorIkea;
+
+
+public class MainCodificador {
+	public static void main(String[] args) throws IOException {
+		//Codificador codec = new Codificador();
+		String original = "7";
+		//System.out.println(original);
+		//String codificado = codec.codificar(original);
+		//System.out.println("Codificado: " + codificado);
+		//String decodificado = codec.decodificar(codificado);
+		//System.out.println("Decodificado: " + decodificado);
+
+	}
+}
